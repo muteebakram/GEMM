7f1bef918a170f42e858747b08163de9c473652c
Muteeb Akram <muteebakram@gmail.com>
2023-12-16 03:31:30 -0700

    cpu batch file

---
+++ openmp/sbatch_lonepeak_cpu.bat
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+#SBATCH -M lonepeak
+#SBATCH --account=owner-guest
+#SBATCH --partition=lonepeak-guest
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH -C c20
+#SBATCH -c 20
+#SBATCH --exclusive
+#SBATCH -t 0:05:00
+#SBATCH --exclude lp[076-082]
+
+echo "*** Assigned Lonepeak Node: " $SLURMD_NODENAME | tee -a lonepeak_mm4_cpu.$SLURM_JOB_ID\.log
+echo " "
+./openmp.sh | tee -a lonepeak_mm4_cpu.$SLURM_JOB_ID\.log
