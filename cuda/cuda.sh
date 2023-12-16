f6d5136e51410773b0cbcd924b3d1d9c642f774b
Muteeb Akram <muteebakram@gmail.com>
2023-12-16 04:08:43 -0700

    openmp cpu results

--- cuda/cuda.sh
+++ cuda/cuda.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 GCC=nvcc
-OUTPUT=AtBt_result_new.txt
 
-# Compare previous run with new run.
-mv $OUTPUT AtBt_result_prev.txt
+OUTPUT=
+# OUTPUT=ABt_result_new.txt
 
-# Clear the old contents.
->$OUTPUT
+if [ ! -z "${OUTPUT}" ]; then
+  # mv $OUTPUT ABt_result_prev.txt # Compare previous run with new run.
+  >$OUTPUT # Clear the old contents.
+fi
 
 function run() {
-  # $GCC -O3 mm4_main.cu mm4_gpu_ab.cu  mm4_gpu_abT.cu  mm4_gpu_aTb.cu  mm4_gpu_aTbT.cu && time ./a.out $1 $2 $3
-  $GCC -O3 mm4_main.cu mm4_gpu_ab.cu  mm4_gpu_abT.cu  mm4_gpu_aTb.cu  mm4_gpu_aTbT.cu && time ./a.out $1 $2 $3 | tee -a $OUTPUT
+  if [ ! -z "${OUTPUT}" ]; then
+    $GCC -O3 mm4_main.cu mm4_gpu_ab.cu mm4_gpu_abT.cu mm4_gpu_aTb.cu mm4_gpu_aTbT.cu && ./a.out $1 $2 $3 | tee -a $OUTPUT
+  else
+    $GCC -O3 mm4_main.cu mm4_gpu_ab.cu mm4_gpu_abT.cu mm4_gpu_aTb.cu mm4_gpu_aTbT.cu && ./a.out $1 $2 $3
+  fi
 }
 
 # Multiple sizes
-# run $((8 * 1024)) $((8 * 1024)) 16
-# run 4096 4096 64
-# run 2048 2048 256
-# run 1024 1024 1024
-# run 256 256 $((16 * 1024))
+run $((8 * 1024)) $((8 * 1024)) 16
+run 4096 4096 64
+run 2048 2048 256
+run 1024 1024 1024
+run 256 256 $((16 * 1024))
 run 64 64 $((256 * 1024))
 run 16 16 $((4 * 1024 * 1024))
 
